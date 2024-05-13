0aec8c44927bf677b15d8956fee08c4e0bcc58cf
Ewelina Gajewska <50328193+Ewelina04@users.noreply.github.com>
2024-05-13 06:51:33 +0200

    Update app_nms.py
     # '(Anti)-heroes', old function

--- app_nms.py
+++ app_nms.py
@@ -7781,7 +7781,7 @@ with st.sidebar:
             contents_radio_an_cat = st.radio("Analytics Units", ('Sentence-based', 'Entity-based'))#, 'Time-based'
             add_spacelines(1)
             if contents_radio_an_cat == 'Entity-based':
-                contents_radio3 = st.radio("Analytics", {'(Anti)-heroes',  "Villain Frequency",  "Villain Score", "Villain Profile", "Villain WordCloud"})
+                contents_radio3 = st.radio("Analytics", { "Villain Frequency",  "Villain Score", "Villain Profile", "Villain WordCloud"}) # '(Anti)-heroes', 
             else:
                 contents_radio3 = st.radio("Analytics", ('Distribution', 'WordCloud'))
             #add_spacelines(1)
@@ -8004,7 +8004,7 @@ with st.sidebar:
             contents_radio_an_cat = st.radio("Analytics Units", ('Sentence-based', 'Entity-based'))
             add_spacelines(1)
             if contents_radio_an_cat == 'Entity-based':
-                contents_radio3 = st.radio("Analytics", ['(Anti)-heroes', "Villain Frequency", "Villain Score", "Villain Profile", "Villain WordCloud",  "Fellows-Devils", ]) # "Polarising Tendency" "Rhetoric Strategies",  "Profiles",
+                contents_radio3 = st.radio("Analytics", ["Villain Frequency", "Villain Score", "Villain Profile", "Villain WordCloud",  "Fellows-Devils", ]) # "Polarising Tendency" "Rhetoric Strategies",  "Profiles",  # '(Anti)-heroes', 
             else:
                 contents_radio3 = st.radio("Analytics", ("Corpora Summary", 'Distribution', 'WordCloud', 'Frequency Tables',
                                             'Odds ratio', 'Pronouns')) # , 'Explore corpora'
@@ -8290,7 +8290,7 @@ with st.sidebar:
         contents_radio_an_cat = st.radio("Analytics Units", ('ADU-based', 'Relation-based', 'Entity-based'))
         add_spacelines(1)
         if contents_radio_an_cat == 'Entity-based':
-            contents_radio3 = st.radio("Analytics", ['(Anti)-heroes', "Villain Frequency", "Villain Score", "Villain Profile", "Villain WordCloud", ])#"Profiles"
+            contents_radio3 = st.radio("Analytics", [ "Villain Frequency", "Villain Score", "Villain Profile", "Villain WordCloud", ])#"Profiles"   # '(Anti)-heroes', 
         else:
             contents_radio3 = st.radio("Analytics", ("Corpora Summary", 'Distribution', 'WordCloud', 'Frequency Tables', 'Odds Ratio', 'Lexical Stats'))# , 'Odds ratio', 'Explore corpora'
         #add_spacelines(1)
